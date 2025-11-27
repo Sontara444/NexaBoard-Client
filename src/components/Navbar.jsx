18867e595b1afd661428e2a52d23bba12cefdf6e
Sontara444 <sontararajput@gmail.com>
2025-11-27 13:11:19 +0530

    small change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({ toggleSidebar }) => {
 
   const dropdownRef = useRef(null);
 
-  // Close dropdown when clicking outside
+
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
